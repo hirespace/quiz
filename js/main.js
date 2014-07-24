caf7819caa16702326feefc7900fe6c7ecae072f
Perry Harlock <perry@hirespace.com>
2014-07-24 14:24:03 +0100

    Lots and lots of styling to radio buttons

--- js/main.js
+++ js/main.js
@@ -59,12 +59,12 @@ $(document).ready(function(){
 	// The final score descriptions
 	function descriptions() {
 		return [
-		"<h5>Spaced Out</h5>Old and Rusty and like the Victoria Tube Carriage at Walthamstow - you're going nowhere fast.",
-		"<h5>Waste of Space</h5>Wuhoh! Like Shoreditch Town Hall, you talk big but you're just a load of empty space.",
-		"<h5>Space Cadet</h5>Like the Safehouses in Peckham, whilst you've got a lot of potential, you just aren't quite there yet. You've let yourself go, peeling paint and a slightly damp smell. You think you're a cool derelict venue - we think you're a run down set of public loos",
-		"<h5>Space Bar</h5>A Clerkenwell Centre convert - you're keen but just a bit uninspired. We advise some sports event themed promotions.",
-		"<h5>Watch this Space</h5>Like Tanner and Co, you're a rising star on the events scene. More practice and fewer scotch eggs, you'll be a winner.",
-		"<h5>Super Space Freak</h5>Like Searcy's at the Gherkin, snotty and you deserve to be."
+		"<h5 class='upper'>Spaced Out</h5>Old and Rusty and like the Victoria Tube Carriage at Walthamstow - you're going nowhere fast.",
+		"<h5 class='upper'>Waste of Space</h5>Wuhoh! Like Shoreditch Town Hall, you talk big but you're just a load of empty space.",
+		"<h5 class='upper'>Space Cadet</h5>Like the Safehouses in Peckham, whilst you've got a lot of potential, you just aren't quite there yet. You've let yourself go, peeling paint and a slightly damp smell. You think you're a cool derelict venue - we think you're a run down set of public loos",
+		"<h5 class='upper'>Space Bar</h5>A Clerkenwell Centre convert - you're keen but just a bit uninspired. We advise some sports event themed promotions.",
+		"<h5 class='upper'>Watch this Space</h5>Like Tanner and Co, you're a rising star on the events scene. More practice and fewer scotch eggs, you'll be a winner.",
+		"<h5 class='upper'>Super Space Freak</h5>Like Searcy's at the Gherkin, snotty and you deserve to be."
 		]
 	}
 	// The answers!
